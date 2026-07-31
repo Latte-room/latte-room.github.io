63b4baf1a409cb825690934c7bfd0fd2cd066cca
Latte-room <0421orange2525@gmail.com>
2026-08-01 04:16:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const CHANNEL_ID = "UCq9s3R0dZ8lW1y6r2JQF6Qg";
 fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&order=date&maxResults=5&part=snippet`)
 .then(res => res.json())
 .then(data => {
+  console.log(data); // 
   let normalVideo = null;
   let shortsVideo = null;
 
